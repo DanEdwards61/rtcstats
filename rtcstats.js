02923129e6e061fd73151d4cd857696b2f3410e8
Philipp Hancke <fippo@appear.in>
2016-06-30 09:33:27 +0200

    drop getstats when not connected

--- rtcstats.js
+++ rtcstats.js
@@ -34,7 +34,7 @@
     args.push(new Date().getTime());
     if (connection.readyState === 1) {
       connection.send(JSON.stringify(args));
-    } else {
+    } else if (args[0] !== 'getstats') {
       buffer.push(args);
     }
   }
